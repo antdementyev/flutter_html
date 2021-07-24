35c35c8f981c2c13f70f1b3b546393468fb1532f
Anton Dementyev <dementyev@subshell.com>
2021-07-24 21:06:59 +0200

    Remove plugins for rendering the <video> tag

--- lib/src/replaced_element.dart
+++ lib/src/replaced_element.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:chewie/chewie.dart';
 import 'package:chewie_audio/chewie_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -14,7 +13,6 @@ import 'package:flutter_html/src/widgets/iframe_unsupported.dart'
 import 'package:flutter_html/style.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:html/dom.dart' as dom;
-import 'package:video_player/video_player.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 /// A [ReplacedElement] is a type of [StyledElement] that does not require its [children] to be rendered.
@@ -136,58 +134,6 @@ class AudioContentElement extends ReplacedElement {
           looping: loop,
           showControls: showControls,
           autoInitialize: true,
-        ),
-      ),
-    );
-  }
-}
-
-/// [VideoContentElement] is a [ContentElement] with a video file as its content.
-class VideoContentElement extends ReplacedElement {
-  final List<String?> src;
-  final String? poster;
-  final bool showControls;
-  final bool autoplay;
-  final bool loop;
-  final bool muted;
-  final double? width;
-  final double? height;
-
-  VideoContentElement({
-    required String name,
-    required this.src,
-    required this.poster,
-    required this.showControls,
-    required this.autoplay,
-    required this.loop,
-    required this.muted,
-    required this.width,
-    required this.height,
-    required dom.Element node,
-  }) : super(name: name, style: Style(), node: node, elementId: node.id);
-
-  @override
-  Widget toWidget(RenderContext context) {
-    final double _width = width ?? (height ?? 150) * 2;
-    final double _height = height ?? (width ?? 300) / 2;
-    return AspectRatio(
-      aspectRatio: _width / _height,
-      child: Container(
-        key: AnchorKey.of(context.parser.key, this),
-        child: Chewie(
-          controller: ChewieController(
-            videoPlayerController: VideoPlayerController.network(
-              src.first ?? "",
-            ),
-            placeholder: poster != null
-                ? Image.network(poster!)
-                : Container(color: Colors.black),
-            autoPlay: autoplay,
-            looping: loop,
-            showControls: showControls,
-            autoInitialize: true,
-            aspectRatio: _width / _height,
-          ),
         ),
       ),
     );
@@ -320,26 +266,6 @@ ReplacedElement parseReplacedElement(
         name: "img",
         src: element.attributes['src'],
         alt: element.attributes['alt'],
-        node: element,
-      );
-    case "video":
-      final sources = <String?>[
-        if (element.attributes['src'] != null) element.attributes['src'],
-        ...ReplacedElement.parseMediaSources(element.children),
-      ];
-      if (sources.isEmpty || sources.first == null) {
-        return EmptyContentElement();
-      }
-      return VideoContentElement(
-        name: "video",
-        src: sources,
-        poster: element.attributes['poster'],
-        showControls: element.attributes['controls'] != null,
-        loop: element.attributes['loop'] != null,
-        autoplay: element.attributes['autoplay'] != null,
-        muted: element.attributes['muted'] != null,
-        width: double.tryParse(element.attributes['width'] ?? ""),
-        height: double.tryParse(element.attributes['height'] ?? ""),
         node: element,
       );
     case "svg":
